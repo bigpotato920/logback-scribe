a817c71339ace068bcae8f6c0a4a7964f21cf8a3
dr.allgood@web.de <dr.allgood@web.de@a284546c-f6ba-a68e-7eac-836454e4103a>
2011-01-27 19:05:41 +0000

    [maven-release-plugin]  copy for tag logback-scribe-1.0.0

---
+++ tags/logback-scribe-1.0.0/src/main/java/de/bitzeche/logback/scribe/ScribeAppender.java
@@ -0,0 +1,170 @@
+/**
+ * Copyright (C) 2011 Bitzeche GmbH <info@bitzeche.de>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.bitzeche.logback.scribe;
+
+/**
+ * Original code from Alex Loddengaard for log4j (https://github.com/alexlod/scribe-log4j-appender)
+ */
+
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TTransportException;
+
+import com.facebook.fb303.LogEntry;
+import com.facebook.fb303.scribe.Client;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.Layout;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.net.Socket;
+import java.io.IOException;
+
+public class ScribeAppender extends AppenderBase<ILoggingEvent> {
+	
+	private List<LogEntry> logEntries;
+	private String hostname;
+	private String scribeHost = "127.0.0.1";
+	private int scribePort = 1463;
+	private String scribeCategory = "scribe";
+
+	private Client client;
+	private TFramedTransport transport;
+
+	private Layout<ILoggingEvent> layout;
+
+	public void start() {
+		int errorCount = 0;
+		try {
+			synchronized (this) {
+				if (hostname == null) {
+					try {
+						hostname = InetAddress.getLocalHost()
+								.getCanonicalHostName();
+					} catch (UnknownHostException e) {
+						// can't get hostname
+						addError("The Hostanme option is mandatory");
+						errorCount++;
+					}
+				}
+
+				// Thrift boilerplate code
+				logEntries = new ArrayList<LogEntry>(1);
+				TSocket sock = new TSocket(new Socket(scribeHost, scribePort));
+				transport = new TFramedTransport(sock);
+				TBinaryProtocol protocol = new TBinaryProtocol(transport,
+						false, false);
+				client = new Client(protocol, protocol);
+
+				if (errorCount == 0) {
+					super.start();
+				}
+			}
+		} catch (TTransportException e) {
+			System.err.println(e);
+		} catch (UnknownHostException e) {
+			System.err.println(e);
+		} catch (IOException e) {
+			System.err.println(e);
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
+
+	public void stop() {
+		if (transport != null && transport.isOpen()) {
+			transport.close();
+		}
+		super.stop();
+	}
+
+	/*
+	 * Appends a log message to Scribe
+	 */
+	@Override
+	public void append(ILoggingEvent loggingEvent) {
+		if (!isStarted()) {
+			return;
+		}
+
+		try {
+			String msg = layout.doLayout(loggingEvent);
+			LogEntry entry = new LogEntry(scribeCategory, msg);
+			logEntries.add(entry);
+			client.Log(logEntries);
+		} catch (TTransportException e) {
+			transport.close();
+		} catch (Exception e) {
+			System.err.println(e);
+		} finally {
+			logEntries.clear();
+		}
+	}
+
+	public void setLayout(Layout<ILoggingEvent> layout) {
+		this.layout = layout;
+	}
+	
+	public String getScribeHost() {
+		return scribeHost;
+	}
+
+	public void setScribeHost(String scribeHost) {
+		this.scribeHost = scribeHost;
+	}
+
+	public int getScribePort() {
+		return scribePort;
+	}
+
+	public void setScribePort(int scribePort) {
+		this.scribePort = scribePort;
+	}
+
+	public String getScribeCategory() {
+		return scribeCategory;
+	}
+
+	public void setScribeCategory(String scribeCategory) {
+		this.scribeCategory = scribeCategory;
+	}
+
+	public TFramedTransport getTransport() {
+		return transport;
+	}
+
+	public void setTransport(TFramedTransport transport) {
+		this.transport = transport;
+	}
+
+	public Layout<ILoggingEvent> getLayout() {
+		return layout;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+}
